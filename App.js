b444300ee19d68ea0b7e44b010605bfc209be4b5
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-09-09 23:46:07 +0300

    nesting navigators

--- App.js
+++ App.js
@@ -6,9 +6,34 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import FavoritesScreen from './screens/FavoritesScreen';
 
 
 const Stack = createNativeStackNavigator()
+
+const Drawer = createDrawerNavigator()
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: '#351401' },
+        headerTintColor: 'white',
+        sceneContainerStyle: { backgroundColor: '#3f2f25' }
+      }}
+    >
+      <Drawer.Screen
+        name={"Categories"}
+        component={CategoriesScreen}
+        options={{
+          title: "All Categories"
+        }}
+      />
+      <Drawer.Screen name={"Favorites"} component={FavoritesScreen} />
+    </Drawer.Navigator>
+  )
+}
 
 export default function App() {
 
@@ -24,18 +49,17 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name={"MealsCategories"}
-            component={CategoriesScreen}
+            name={"Drawer"}
+            // component={CategoriesScreen}
+            component={DrawerNavigator}
             options={{
-              title: "All Categories",
+              headerShown: false
             }}
           />
           <Stack.Screen
             name={"MealsOverview"}
             component={MealsOverviewScreen}
           // options={({ route, navigation }) => {
-          //   console.log(route)
-          //   console.log(navigation)
           //   const catId = route.params.title
           //   return { title: catId }
           // }}
@@ -43,13 +67,16 @@ export default function App() {
           <Stack.Screen
             name={"MealDetail"}
             component={MealDetailScreen}
-          // options={{
-          //   headerRight: () => {
-          //     return (
-          //       <Button title={"Tap me!"} onPress={ } />
-          //     )
-          //   }
-          // }}
+            // options={{
+            //   headerRight: () => {
+            //     return (
+            //       <Button title={"Tap me!"} onPress={ } />
+            //     )
+            //   }
+            // }}
+            options={{
+              title: "About the Meal"
+            }}
           />
         </Stack.Navigator>
       </NavigationContainer>
--- components/IconButton.js
+++ components/IconButton.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native'
+import { StyleSheet, Pressable } from 'react-native'
 import React from 'react'
 import { Ionicons } from '@expo/vector-icons'
 
